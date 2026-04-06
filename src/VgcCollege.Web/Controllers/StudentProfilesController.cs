2cfd1c942fa765267a13d2a7ad846530c7ac0374
Milena Pires <milena.pires41@gmail.com>
2026-04-06 12:12:49 +0100

    Add controllers data

---
+++ src/VgcCollege.Web/Controllers/StudentProfilesController.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VgcCollege.Web.Data;
+using VgcCollege.Web.Models;
+
+namespace VgcCollege.Web.Controllers
+{
+    public class StudentProfilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentProfilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StudentProfiles
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.StudentProfiles.Include(s => s.IdentityUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: StudentProfiles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentProfile = await _context.StudentProfiles
+                .Include(s => s.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentProfile);
+        }
+
+        // GET: StudentProfiles/Create
+        public IActionResult Create()
+        {
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: StudentProfiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdentityUserId,Name,Email,Phone,Address,StudentNumber,DOB")] StudentProfile studentProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(studentProfile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", studentProfile.IdentityUserId);
+            return View(studentProfile);
+        }
+
+        // GET: StudentProfiles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentProfile = await _context.StudentProfiles.FindAsync(id);
+            if (studentProfile == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", studentProfile.IdentityUserId);
+            return View(studentProfile);
+        }
+
+        // POST: StudentProfiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdentityUserId,Name,Email,Phone,Address,StudentNumber,DOB")] StudentProfile studentProfile)
+        {
+            if (id != studentProfile.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(studentProfile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentProfileExists(studentProfile.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", studentProfile.IdentityUserId);
+            return View(studentProfile);
+        }
+
+        // GET: StudentProfiles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentProfile = await _context.StudentProfiles
+                .Include(s => s.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentProfile);
+        }
+
+        // POST: StudentProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var studentProfile = await _context.StudentProfiles.FindAsync(id);
+            if (studentProfile != null)
+            {
+                _context.StudentProfiles.Remove(studentProfile);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentProfileExists(int id)
+        {
+            return _context.StudentProfiles.Any(e => e.Id == id);
+        }
+    }
+}
